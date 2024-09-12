79b9a2bd43b338ae418450b3003a51b75692acbf
ridwaanhall <erbyl14@gmail.com>
2024-09-12 20:28:11 +0700

    read api from spacex and show html

--- base/urls.py
+++ base/urls.py
@@ -2,8 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('text/', views.simple_text_view, name='simple_text_view'),
-    path('json/', views.simple_json_view, name='simple_json_view'),
-    path('no-content/', views.no_content_view, name='no_content_view'),
-    path('redirect/', views.simple_redirect_view, name='simple_redirect_view'),
+    path('', views.follow_dragon_view, name='follow_dragon'),
+    path('dragon_public.json', views.dragon_public, name='dragon_public'),
 ]
