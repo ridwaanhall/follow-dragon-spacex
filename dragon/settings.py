3beb00a878b1b30da87e3a674cc8fe9124a47475
ridwaanhall <erbyl14@gmail.com>
2024-09-28 17:09:19 +0700

    static root

--- dragon/settings.py
+++ dragon/settings.py
@@ -131,6 +131,8 @@ STATICFILES_DIRS = [
     BASE_DIR / 'base' / 'static', # 2 version
 ]
 
+STATIC_ROOT = BASE_DIR / 'static'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
