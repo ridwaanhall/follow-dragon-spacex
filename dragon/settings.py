850063587342528ae9b887b9bfcef937e5f64dee
Ridwan Halim <91854659+ridwaanhall@users.noreply.github.com>
2025-03-16 08:13:06 +0000

    refactor: update template directory path in settings

--- dragon/settings.py
+++ dragon/settings.py
@@ -67,7 +67,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            BASE_DIR / 'base/templates',
+            BASE_DIR / 'templates',
         ],
         'APP_DIRS': True,
         'OPTIONS': {
